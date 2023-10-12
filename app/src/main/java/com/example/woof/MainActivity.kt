dd2eca6b520994c66e36b663b643ce5622f3a00c
ngthgph <ngthgph112@gmail.com>
2023-10-12 16:08:45 +0700

    Experiment with other animations

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -21,10 +21,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -136,7 +138,11 @@ fun DogItem(
     modifier: Modifier = Modifier
 ) {
     var expanded by remember { mutableStateOf(false) }
-
+    val color by animateColorAsState(
+        targetValue =
+        if (expanded) MaterialTheme.colorScheme.tertiaryContainer
+        else MaterialTheme.colorScheme.primaryContainer,
+    )
     Card(modifier) {
         Column(
             modifier = Modifier
@@ -146,6 +152,7 @@ fun DogItem(
                         stiffness = Spring.StiffnessMedium
                     )
                 )
+                .background(color = color)
         ) {
             Row(
                 modifier = Modifier
